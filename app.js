3ec711d9a0f2f71cbab2717ae328fc6b20b6b0f8
rweaver <robertgweaver@icloud.com>
2021-09-30 19:19:08 -0700

    added index.html and app.js file, linked js files to html

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+
+//what do we need to do to create game?
+// 1) get a word from the dictionary array that is 6 letters long
+// 2) go through and find all possible words from subsets of letters that are at least 3 letters long
+    // would be good to save all possible words to array. would take awhile at start of game but make for smooth gameplay
+// 3) display the root word at the top of the console with letters scrambled
+// 4) display the list of sub words with the letters hidden by "-"
+// 5) game loop that continuously prompts users for input and will alert user of state of their guess
