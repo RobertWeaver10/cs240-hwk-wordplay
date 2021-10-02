1b2d50771b5cec95380afdbef400a880dc689742
rweaver <robertgweaver@icloud.com>
2021-10-02 14:31:18 -0700

    wrote fn to get words length 3-6 and list of root words

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("hello world")
-
 //what do we need to do to create game?
 // 1) get a word from the dictionary array that is 6 letters long
 // 2) go through and find all possible words from subsets of letters that are at least 3 letters long
@@ -7,3 +5,30 @@ console.log("hello world")
 // 3) display the root word at the top of the console with letters scrambled
 // 4) display the list of sub words with the letters hidden by "-"
 // 5) game loop that continuously prompts users for input and will alert user of state of their guess
+let gameDict = [];
+let rootWords = [];
+
+function gameDictionary(){
+    for (let i = 0; i < dictionary.length; i++){
+        if(dictionary[i].length == 6){
+            rootWords.push(dictionary[i]);
+        }
+        if(dictionary[i].length > 2 && dictionary[i].length < 7){
+            gameDict.push(dictionary[i]);
+        }
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRootWord(){
+    let rootWord = rootWords[getRandomInt(rootWords.length)];
+    return rootWord;
+}
+
+gameDictionary();
+console.log(rootWords.length);
+console.log(gameDict.length);
+console.log(getRootWord());
